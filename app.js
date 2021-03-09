8dd5378af6671d82279aa9cec50c5fc635bc4505
Goblin820 <ps911107@naver.com>
2021-03-10 00:09:55 +0900

    1. 로그인 체크 기본틀 추가

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ const { sequelize } = require('./models/index');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+const filesRouter = require('./routes/files');
 
+// 서버 설정
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
@@ -20,13 +23,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 sequelize
-    .sync({ force: false })
-    .then(() => {
-        console.log('데이터베이스 연결 성공');
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+	.sync({ force: false })
+	.then(() => {
+		console.log('데이터베이스 연결 성공');
+	})
+	.catch((err) => {
+		console.error(err);
+	});
 
 // 패키지 모듈 라우터 사용 설정
 app.use(logger('dev'));
@@ -36,37 +39,38 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(pugStatic('#{__dirname}/public/'));
 app.use(
-    session({
-        secret: 'keyboard cat',
-        resave: false,
-        saveUninitialized: true,
-    })
+	session({
+		secret: 'keyboard cat',
+		resave: false,
+		saveUninitialized: true,
+	})
 );
 
 // 라우터 사용 설정
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+app.use('/files', filesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-    error.status = 404;
-    next(error);
+	const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+	error.status = 404;
+	next(error);
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = process.env.NODE_ENV !== 'dbKPP' ? err : {};
+	res.locals.message = err.message;
+	res.locals.error = process.env.NODE_ENV !== 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('./error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('./error');
 });
 
 app.listen(app.get('port'), function () {
-    console.log(app.get('port'), '번 포트에서 대기 중');
+	console.log(app.get('port'), '번 포트에서 대기 중');
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -5,51 +5,51 @@ const User = require('../models/users');
 
 /* GET users listing. */
 router
-    .route('/')
-    // 모든 유저의 데이터 받아오기
-    .get(async function (req, res, next) {
-        try {
-            const users = await User.findAll();
-        } catch (error) {
-            console.log(error);
-            next(error);
-        }
-    })
-    // 유저 추가(회원가입)
-    .post(async function (req, res, next) {
-        try {
-        } catch (error) {
-            console.log(error);
-            next(error);
-        }
-    })
-    // 유저 조회(단일)
-    .search(async function (req, res, next) {
-        try {
-        } catch (error) {
-            console.log(error);
-            next(error);
-        }
-    })
-    // 유저 삭제
-    .delete(async function (req, res, next) {
-        try {
-        } catch (error) {
-            console.log(error);
-            next(error);
-        }
-    });
+	.route('/')
+	// 모든 유저의 데이터 받아오기
+	.get(async function (req, res, next) {
+		try {
+			const users = await User.findAll();
+		} catch (error) {
+			console.log(error);
+			next(error);
+		}
+	})
+	// 유저 추가(회원가입)
+	.post(async function (req, res, next) {
+		try {
+		} catch (error) {
+			console.log(error);
+			next(error);
+		}
+	})
+	// 유저 조회(단일)
+	.search(async function (req, res, next) {
+		try {
+		} catch (error) {
+			console.log(error);
+			next(error);
+		}
+	})
+	// 유저 삭제
+	.delete(async function (req, res, next) {
+		try {
+		} catch (error) {
+			console.log(error);
+			next(error);
+		}
+	});
 
 // 로그인 확인
 router.get('/loginCheck', async function (req, res, next) {
-    try {
-        if (req.session.userId) {
-            res.status(200).send({ result: true });
-        } else {
-            res.status(200).send({ result: false });
-        }
-    } catch (error) {
-        next(error);
-    }
+	try {
+		if (req.session.userId) {
+			res.status(200).send({ result: true });
+		} else {
+			res.status(200).send({ result: false });
+		}
+	} catch (error) {
+		next(error);
+	}
 });
 module.exports = router;
