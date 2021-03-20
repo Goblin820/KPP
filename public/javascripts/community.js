3594f014712cf070659ba6ed726e4966cadf0a76
Goblin820 <ps911107@naver.com>
2021-03-20 11:39:46 +0900

    1. 불필요 파일 및 라이브러리 삭제 2. Datatables 라이브러리 추가

--- public/javascripts/community.js
+++ public/javascripts/community.js
@@ -2,6 +2,8 @@ window.addEventListener('load', function (e) {
 	community_initTables();
 });
 
-function community_initTables() {}
+function community_initTables() {
+	// $('#board-table').Datatable();
+}
 
 function community_getTablesData() {}
