e41eab316257ddb909c8df574de5b7c368a5317f
Goblin820 <ps911107@naver.com>
2021-03-25 03:10:06 +0900

    1. kpop 페이지 일단 완성

--- public/javascripts/kpop.js
+++ public/javascripts/kpop.js
@@ -3,10 +3,18 @@ const kpopObj = {
 		$element: null,
 		handler: null,
 		height: 0,
-		speed: 2,
+		speed: 5,
+		stopTimer: 500,
 	},
 	lineChecker: {
 		handler: null,
+		current: 0,
+	},
+	history: {
+		$content: null,
+	},
+	scroll: {
+		isStart: false,
 	},
 };
 const $ELEMENT = $('div');
@@ -19,6 +27,7 @@ window.addEventListener('load', function (e) {
 
 function kpop_initObj() {
 	kpopObj.line.$element = $('.line');
+	kpopObj.history.$content = $('.history-content');
 }
 
 function kpop_lineAnim() {
@@ -26,6 +35,53 @@ function kpop_lineAnim() {
 
 	kpopObj.line.$element.height(kpopObj.line.height + kpopObj.line.speed);
 
-	kpopObj.line.animHandeler = requestAnimationFrame(kpop_lineAnim);
+	window.scrollTo(0, kpopObj.line.$element.height() - 400);
+
+	kpopObj.line.handler = requestAnimationFrame(kpop_lineAnim);
 }
-function kpop_lineCheck() {}
+function kpop_lineCheck() {
+	if (kpopObj.lineChecker.current == 0) {
+		if (kpopObj.line.height > 300) {
+			kpop_routine();
+		}
+	} else if (kpopObj.lineChecker.current == 1) {
+		if (kpopObj.line.height > 600) {
+			kpop_routine();
+		}
+	} else if (kpopObj.lineChecker.current == 2) {
+		if (kpopObj.line.height > 900) {
+			kpop_routine();
+		}
+	} else if (kpopObj.lineChecker.current == 3) {
+		if (kpopObj.line.height > 1200) {
+			kpop_routine();
+		}
+	} else if (kpopObj.lineChecker.current == 4) {
+		if (kpopObj.line.height > 1500) {
+			kpop_routine();
+		}
+	} else if (kpopObj.lineChecker.current == 5) {
+		if (kpopObj.line.height > 1800) {
+			kpop_stopLine();
+			kpop_onContent();
+		}
+	}
+}
+function kpop_routine() {
+	kpop_onContent();
+	kpop_stopLine();
+	kpop_moveLine(kpopObj.line.stopTimer);
+}
+function kpop_moveLine(timer) {
+	setTimeout(() => {
+		kpopObj.line.handler = requestAnimationFrame(kpop_lineAnim);
+	}, timer);
+}
+function kpop_stopLine() {
+	cancelAnimationFrame(kpopObj.line.handler);
+}
+
+function kpop_onContent() {
+	$('.history-content')[kpopObj.lineChecker.current].classList.add('on-content');
+	kpopObj.lineChecker.current++;
+}
