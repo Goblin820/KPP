45bd05cf31d5b0b53c71d2d0d72ff278eebd77b2
Goblin820 <ps911107@naver.com>
2021-03-30 00:36:04 +0900

    1. 이미지 업로드 기능 작업중

--- public/javascripts/writePage.js
+++ public/javascripts/writePage.js
@@ -1,118 +1,118 @@
 const writePageObj = {
-    summerNoteElem: null,
+	summerNoteElem: null,
 };
 
 window.addEventListener('load', function (e) {
-    writePage_initEvent();
-    writePage_initSummerNote();
+	writePage_initEvent();
+	writePage_initSummerNote();
 });
 
 function writePage_initEvent() {
-    document.getElementById('submit').addEventListener('click', writePage_onClickSubmitBtn);
+	document.getElementById('submit').addEventListener('click', writePage_onClickSubmitBtn);
 }
 
 function writePage_initSummerNote() {
-    writePageObj.summerNoteElem = $('#summer-note').summernote({
-        lang: 'ko-KR',
-        height: '37vh',
-        // placeholder: '최대 2048자까지 입력이 가능합니다',
-        toolbar: [
-            // [groupName, [list of button]]
-            ['Font Style', ['fontname']],
-            ['style', ['bold', 'italic', 'underline']],
-            ['font', ['strikethrough']],
-            ['fontsize', ['fontsize']],
-            ['color', ['forecolor', 'backcolor']],
-            ['para', ['ul', 'ol', 'paragraph']],
-            ['height', ['height']],
-            ['insert', ['picture', 'link', 'video']],
-            ['help', ['help']],
-        ],
-        fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', '맑은 고딕', '궁서', '굴림체', '굴림', '돋움체', '바탕체'],
-        fontSizes: ['8', '9', '10', '11', '12', '14', '16', '18', '20', '22', '24', '28', '30', '36', '50', '72'],
+	writePageObj.summerNoteElem = $('#summer-note').summernote({
+		lang: 'ko-KR',
+		height: '37vh',
+		// placeholder: '최대 2048자까지 입력이 가능합니다',
+		toolbar: [
+			// [groupName, [list of button]]
+			['Font Style', ['fontname']],
+			['style', ['bold', 'italic', 'underline']],
+			['font', ['strikethrough']],
+			['fontsize', ['fontsize']],
+			['color', ['forecolor', 'backcolor']],
+			['para', ['ul', 'ol', 'paragraph']],
+			['height', ['height']],
+			['insert', ['picture', 'link', 'video']],
+			['help', ['help']],
+		],
+		fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', '맑은 고딕', '궁서', '굴림체', '굴림', '돋움체', '바탕체'],
+		fontSizes: ['8', '9', '10', '11', '12', '14', '16', '18', '20', '22', '24', '28', '30', '36', '50', '72'],
 
-        callbacks: {
-            onImageUpload: function (files, editor, welEditable) {
-                if (files.length) {
-                    for (let i = 0; i < files.length; i++) {
-                        write_sendImageFile(files[i], this);
-                    }
-                }
-            },
-            // onPaste: function (e) {
-            // 	var clipboardData = e.originalEvent.clipboardData;
-            // 	if (clipboardData && clipboardData.items && clipboardData.items.length) {
-            // 		var item = clipboardData.items[0];
-            // 		if (item.kind === 'file' && item.type.indexOf('image/') !== -1) {
-            // 			e.preventDefault();
-            // 		}
-            // 	}
-            // }
-        },
-    });
+		callbacks: {
+			onImageUpload: function (files, editor, welEditable) {
+				if (files.length) {
+					for (let i = 0; i < files.length; i++) {
+						write_sendImageFile(files[i], this);
+					}
+				}
+			},
+			// onPaste: function (e) {
+			// 	var clipboardData = e.originalEvent.clipboardData;
+			// 	if (clipboardData && clipboardData.items && clipboardData.items.length) {
+			// 		var item = clipboardData.items[0];
+			// 		if (item.kind === 'file' && item.type.indexOf('image/') !== -1) {
+			// 			e.preventDefault();
+			// 		}
+			// 	}
+			// }
+		},
+	});
 }
 function writePage_onClickSubmitBtn(e) {
-    e.preventDefault();
+	e.preventDefault();
 
-    const title = document.getElementsByClassName('title-text')[0].value;
-    if (title == '') {
-        Swal.fire({
-            icon: 'error',
-            text: '제목을 입력해주세요!',
-        });
-        return;
-    }
-    const text = writePageObj.summerNoteElem.summernote('code');
+	const title = document.getElementsByClassName('title-text')[0].value;
+	if (title == '') {
+		Swal.fire({
+			icon: 'error',
+			text: '제목을 입력해주세요!',
+		});
+		return;
+	}
+	const text = writePageObj.summerNoteElem.summernote('code');
 
-    if (text == '') {
-        Swal.fire({
-            icon: 'error',
-            text: '본문을 입력해주세요!',
-        });
-        return;
-    }
+	if (text == '') {
+		Swal.fire({
+			icon: 'error',
+			text: '본문을 입력해주세요!',
+		});
+		return;
+	}
 
-    // 서버에 게시 글 post 요청
-    const postData = {
-        title: title,
-        text: text,
-        author: document.getElementById('author').textContent,
-    };
+	// 서버에 게시 글 post 요청
+	const postData = {
+		title: title,
+		text: text,
+		author: document.getElementById('author').textContent,
+	};
 
-    fetch('/board', {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(postData),
-    }).then(function (res) {
-        console.log(res);
-        if (res.status == 200) {
-            Swal.fire({
-                icon: 'success',
-                text: '글쓰기 성공!',
-            }).then(function () {
-                window.location.href = '/community';
-            });
-        }
-    });
+	fetch('/board', {
+		method: 'post',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(postData),
+	}).then(function (res) {
+		console.log(res);
+		if (res.status == 200) {
+			Swal.fire({
+				icon: 'success',
+				text: '글쓰기 성공!',
+			}).then(function () {
+				window.location.href = '/community';
+			});
+		}
+	});
 }
 
 function write_sendImageFile(file, element) {
-    // file.type
+	// file.type
 
-    const formData = new FormData();
-    formData.append('file', file);
-    $.ajax({
-        url: '/files/file/image',
-        type: 'POST',
-        cache: false,
-        contentType: false,
-        enctype: 'multipart/form-data',
-        processData: false,
-        data: formData,
-        success: function (res) {
-            console.log(res);
-        },
-    });
+	const formData = new FormData();
+	formData.append('img', file);
+	$.ajax({
+		url: '/files/file/image',
+		type: 'POST',
+		cache: false,
+		contentType: false,
+		enctype: 'multipart/form-data',
+		processData: false,
+		data: formData,
+		success: function (res) {
+			console.log(res);
+		},
+	});
 }
--- routes/files.js
+++ routes/files.js
@@ -9,133 +9,134 @@ const path = require('path');
 
 // 요청한 경로의 폴더 내부에 있는 모든 파일 데이터를 전달해주는 라우트
 router.get('/dir', async function (req, res, next) {
-    const dir = req.query.dir;
-    try {
-        // 경로가 존재하는지 확인한다.
-        if (fs.existsSync(dir)) {
-            // 경로안의 파일들의 이름을 읽어온다.
-            const readFilesName = await fs.readdirSync(dir);
-            res.status(200).send(readFilesName);
-        } else res.status(204).send(null);
-    } catch (error) {
-        next(error);
-    }
+	const dir = req.query.dir;
+	try {
+		// 경로가 존재하는지 확인한다.
+		if (fs.existsSync(dir)) {
+			// 경로안의 파일들의 이름을 읽어온다.
+			const readFilesName = await fs.readdirSync(dir);
+			res.status(200).send(readFilesName);
+		} else res.status(204).send(null);
+	} catch (error) {
+		next(error);
+	}
 });
 
 // 요청한 경로와 파일명으로 파일 데이터를 전달해주는 라우트
 router.get('/file', function (req, res, next) {
-    const path = req.query.dir + '/' + req.query.fileName;
+	const path = req.query.dir + '/' + req.query.fileName;
 
-    try {
-        if (fs.existsSync(req.query.dir)) {
-            fs.readFile(path, 'utf8', function (err, data) {
-                if (err) {
-                    console.error(err);
-                } else {
-                    res.status(200).send(data);
-                }
-            });
-        } else res.status(204).send(null);
-    } catch (error) {
-        next(error);
-    }
+	try {
+		if (fs.existsSync(req.query.dir)) {
+			fs.readFile(path, 'utf8', function (err, data) {
+				if (err) {
+					console.error(err);
+				} else {
+					res.status(200).send(data);
+				}
+			});
+		} else res.status(204).send(null);
+	} catch (error) {
+		next(error);
+	}
 });
 
 // uploads 폴더가 있는지 확인하고 없으면 생성한다.
 try {
-    fs.readdirSync('./public/uploads');
+	fs.readdirSync('./public/uploads');
 } catch (err) {
-    fs.mkdirSync('./public/uploads');
+	fs.mkdirSync('./public/uploads');
 }
 const upload = multer({
-    storage: multer.diskStorage({
-        destination(req, file, done) {
-            done(null, './public/uploads/');
-        },
-        filename(req, file, done) {
-            const ext = path.extname(file.originalname);
-            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
-        },
-    }),
-    limits: { fileSize: 5 * 1024 * 1024 },
+	storage: multer.diskStorage({
+		destination(req, file, done) {
+			console.log(file);
+			done(null, './public/uploads/');
+		},
+		filename(req, file, done) {
+			const ext = path.extname(file.originalname);
+			done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+		},
+	}),
+	limits: { fileSize: 5 * 1024 * 1024 },
 });
 
-router.post('/file/image', upload.single('jpg'), async function (req, res, next) {
-    try {
-        console.log('file:', req.file);
-    } catch (error) {
-        next(error);
-    }
+router.post('/file/image', upload.single('img'), async function (req, res, next) {
+	try {
+		console.log('file:', req.body);
+	} catch (error) {
+		next(error);
+	}
 });
 
 // 클라이언트에서 웹 스크래핑 요청이 있을 때 호출
 router.get('/scraping', function (req, res, next) {
-    const url = req.query.url;
-    const scrapingType = req.query.type;
+	const url = req.query.url;
+	const scrapingType = req.query.type;
 
-    axios
-        .get(url, {
-            encoding: null,
-            responseType: 'arraybuffer',
-        })
-        .then(function (html) {
-            try {
-                // 컨텐츠 타입 확인 : text/html;charset=EUC-KR
-                const charset = charsetFinder(html.headers);
-                let $ = '';
-                if (charset !== 'utf-8') $ = cheerio.load(iconv.decode(html.data, charset));
-                else $ = cheerio.load(html.data);
+	axios
+		.get(url, {
+			encoding: null,
+			responseType: 'arraybuffer',
+		})
+		.then(function (html) {
+			try {
+				// 컨텐츠 타입 확인 : text/html;charset=EUC-KR
+				const charset = charsetFinder(html.headers);
+				let $ = '';
+				if (charset !== 'utf-8') $ = cheerio.load(iconv.decode(html.data, charset));
+				else $ = cheerio.load(html.data);
 
-                let result = null;
-                switch (scrapingType) {
-                    case 'stock':
-                        result = scrapingStock($);
-                        break;
+				let result = null;
+				switch (scrapingType) {
+					case 'stock':
+						result = scrapingStock($);
+						break;
 
-                    default:
-                        res.status(404).send({ result: 'error' });
-                        break;
-                }
+					default:
+						res.status(404).send({ result: 'error' });
+						break;
+				}
 
-                res.status(200).send(result);
-            } catch (error) {
-                console.error(error);
-            }
-        });
+				res.status(200).send(result);
+			} catch (error) {
+				console.error(error);
+			}
+		});
 });
 
 function charsetFinder(header) {
-    const contentType = header['content-type'];
-    const findText = 'charset=';
-    const findIndex = contentType.indexOf(findText);
-    if (findIndex == -1) return null;
+	const contentType = header['content-type'];
+	const findText = 'charset=';
+	const findIndex = contentType.indexOf(findText);
+	if (findIndex == -1) return null;
 
-    const result = contentType.substring(findIndex + findText.length, contentType.length);
-    return result;
+	const result = contentType.substring(findIndex + findText.length, contentType.length);
+	return result;
 }
 
 function scrapingStock($) {
-    const result = {};
+	const result = {};
 
-    result.name = $('#middle > div.h_company > div.wrap_company > h2 > a').text();
-    result.nowVal = $('#_nowVal').text();
-    result.diffVal = $('#_diff > span')
-        .text()
-        .replace(/(\n\r|\n\t|\r\t|\n|\t|\r)/gm, '');
-    result.rate = $('#_rate > span')
-        .text()
-        .replace(/(\n\r|\n\t|\r\t|\n|\t|\r)/gm, '');
+	result.name = $('#middle > div.h_company > div.wrap_company > h2 > a').text();
+	result.nowVal = $('#_nowVal').text();
+	result.diffVal = $('#_diff > span')
+		.text()
+		.replace(/(\n\r|\n\t|\r\t|\n|\t|\r)/gm, '');
+	result.rate = $('#_rate > span')
+		.text()
+		.replace(/(\n\r|\n\t|\r\t|\n|\t|\r)/gm, '');
 
-    // 숫자 및 ',' 추출
-    // const value = result.variance.replace(/[^0-9,]/g, '');
+	// 숫자 및 ',' 추출
+	// const value = result.variance.replace(/[^0-9,]/g, '');
 
-    // 한글만 추출 (초성 제외)
-    // const text = result.variance.replace(/[^가-힣]/g, '');
+	// 한글만 추출 (초성 제외)
+	// const text = result.variance.replace(/[^가-힣]/g, '');
 
-    // 공백 제거
-    // replace(/(\n\r|\n\t|\r\t|\n|\t|\r)/gm, '');
+	// 공백 제거
+	// replace(/(\n\r|\n\t|\r\t|\n|\t|\r)/gm, '');
 
-    return result;
+	return result;
 }
 
 module.exports = router;
